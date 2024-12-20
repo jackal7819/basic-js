6bd2ba0b9b1574312a0506da8057de2ef45b7130
jackal7819 <jackal3166@gmail.com>
2024-12-20 18:39:32 +0200

    feat: pass next 19 tests (37/83 completed)

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,11 +22,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+	const stats = {};
+
+	domains.forEach((domain) => {
+		const parts = domain.split('.').reverse();
+		let current = '';
+		parts.forEach((part) => {
+			current += '.' + part;
+			stats[current] = stats[current] ? stats[current] + 1 : 1;
+		});
+	});
+
+	return stats;
 }
 
 module.exports = {
-  getDNSStats
+	getDNSStats,
 };
