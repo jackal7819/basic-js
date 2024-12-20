0d3d4fcce1e3ef537bae0e7ef02c788fd56b8564
jackal7819 <jackal3166@gmail.com>
2024-12-20 17:51:57 +0200

    feat: pass first 18 tests out of 83

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,21 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (
+    typeof sampleActivity !== 'string' ||
+    isNaN(parseFloat(sampleActivity)) ||
+    parseFloat(sampleActivity) <= 0 ||
+    parseFloat(sampleActivity) > MODERN_ACTIVITY
+  ) {
+    return false;
+  }
+
+  const activity = parseFloat(sampleActivity);
+  const decayRate = 0.693 / HALF_LIFE_PERIOD;
+  const age = Math.log(MODERN_ACTIVITY / activity) / decayRate;
+
+  return Math.ceil(age);
 }
 
 module.exports = {
